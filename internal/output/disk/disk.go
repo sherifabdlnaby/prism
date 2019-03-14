3a29762514e5f6f4ec4e5dc71b7631abf3c3f03d
Mohamed Refaie <mohamed1refaie@hotmail.com>
2019-03-14 21:06:11 +0200

    Refactoring

--- internal/output/disk/disk.go
+++ internal/output/disk/disk.go
@@ -27,7 +27,7 @@ func (d *Disk) TransactionChan() chan<- types.Transaction {
 	return d.Transactions
 }
 
-// Initialize the disk output plugin
+//Init func Initialize the disk output plugin
 func (d *Disk) Init(config types.Config, logger zap.Logger) error {
 	d.FileName = config["filename"].(string)
 	d.FilePath = config["filepath"].(string)
@@ -37,6 +37,33 @@ func (d *Disk) Init(config types.Config, logger zap.Logger) error {
 	d.stopChan = make(chan struct{})
 	d.logger = logger
 	return nil
+}
+
+//WriteOnDisk func takes the disk and the transaction
+//that to be written on the disk
+func WriteOnDisk(d *Disk, transaction types.Transaction) {
+	defer d.wg.Done()
+	var err error
+	ack := true
+	d.logger.Info("RECEIVED OUTPUT TRANSACTION...")
+	bytes, _ := ioutil.ReadAll(transaction)
+	if _, err = os.Stat(d.FilePath); os.IsNotExist(err) {
+		err = os.MkdirAll(d.FilePath, os.ModePerm)
+	}
+	if err == nil {
+		FilePath := filepath.Join(d.FilePath, d.FileName)
+		err = ioutil.WriteFile(FilePath, bytes, d.FilePermission)
+		d.logger.Info("OUTPUT SUCCESSFUL, Sending Response. ")
+	} else {
+		d.logger.Info("Error in output: ", zap.Error(err))
+		ack = false
+	}
+	// send response
+	transaction.ResponseChan <- types.Response{
+		Error: err,
+		Ack:   ack,
+	}
+	return
 }
 
 // Start the plugin and be ready for taking transactions
@@ -54,38 +81,7 @@ func (d *Disk) Start() error {
 			case transaction, more := <-d.Transactions:
 				if more {
 					d.wg.Add(1)
-					go func() {
-						defer d.wg.Done()
-						d.logger.Info("RECEIVED OUTPUT TRANSACTION...")
-						bytes, _ := ioutil.ReadAll(transaction)
-						var err error
-						if _, err = os.Stat(d.FilePath); os.IsNotExist(err) {
-							err = os.MkdirAll(d.FilePath, os.ModePerm)
-						}
-						if err == nil {
-							FilePath := filepath.Join(d.FilePath, d.FileName)
-							err = ioutil.WriteFile(FilePath, bytes, d.FilePermission)
-						}
-						if err != nil {
-							d.logger.Info("Error in output: ", zap.Error(err))
-							// send response
-							transaction.ResponseChan <- types.Response{
-								Error: err,
-								Ack:   false,
-							}
-							return
-						}
-
-						d.logger.Info("OUTPUT SUCCESSFUL, Sending Response. ")
-
-						// send response
-						transaction.ResponseChan <- types.Response{
-							Error: nil,
-							Ack:   true,
-						}
-
-						return
-					}()
+					go WriteOnDisk(d, transaction)
 				}
 			}
 		}
@@ -94,7 +90,7 @@ func (d *Disk) Start() error {
 	return nil
 }
 
-// Send a close signal to stop chan
+//Close func Send a close signal to stop chan
 // to stop taking transactions and Close everything safely
 func (d *Disk) Close(time.Duration) error {
 	d.logger.Info("Sending closing signal to Disk Output...")
