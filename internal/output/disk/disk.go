dc0041deb92540b1c6e1b09e5bc4efb85aed869f
Mohamed Refaie <mohamed1refaie@hotmail.com>
2019-03-16 23:29:53 +0200

    go fmt

--- internal/output/disk/disk.go
+++ internal/output/disk/disk.go
@@ -31,11 +31,11 @@ func (d *Disk) TransactionChan() chan<- types.Transaction {
 
 //Init func Initialize the disk output plugin
 func (d *Disk) Init(config types.Config, logger zap.Logger) error {
-	if d.FilePath,d.TypeCheck = config["filepath"].(string); !d.TypeCheck {
+	if d.FilePath, d.TypeCheck = config["filepath"].(string); !d.TypeCheck {
 		return errors.New("FilePath must be a string")
 	}
 	path.Clean(d.FilePath)
-	if d.FilePermission,d.TypeCheck = config["permission"].(string); !d.TypeCheck{
+	if d.FilePermission, d.TypeCheck = config["permission"].(string); !d.TypeCheck {
 		return errors.New("FilePermission must be from a string")
 	}
 	d.Transactions = make(chan types.Transaction)
@@ -50,21 +50,21 @@ func (d *Disk) WriteOnDisk(transaction types.Transaction) {
 	defer d.wg.Done()
 	ack := true
 	bytes, err := ioutil.ReadAll(transaction)
-	if err==nil {
-		dir:=filepath.Dir(d.FilePath)
+	if err == nil {
+		dir := filepath.Dir(d.FilePath)
 		if _, err = os.Stat(dir); os.IsNotExist(err) {
 			err = os.MkdirAll(dir, os.ModePerm)
 		}
 	}
 	if err == nil {
-		perm32,errParse:=strconv.ParseUint(d.FilePermission,0,32)
-		err=errParse
+		perm32, errParse := strconv.ParseUint(d.FilePermission, 0, 32)
+		err = errParse
 		if err == nil {
 			permission := os.FileMode(perm32)
 			err = ioutil.WriteFile(d.FilePath, bytes, permission)
 		}
 	}
-	if err!=nil {
+	if err != nil {
 		ack = false
 	}
 	// send response
