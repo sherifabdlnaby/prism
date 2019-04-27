562eaf5f03e1a2eaefc3f7fee1b1ae505f02bb7c
Sherif Abdel-Naby <sherifabdlnaby@gmail.com>
2019-04-27 19:24:20 +0200

    Update Dummy plugin to show correct use of the current interface.

--- app/pipeline/node/node.go
+++ app/pipeline/node/node.go
@@ -4,6 +4,8 @@ import "github.com/sherifabdlnaby/prism/pkg/transaction"
 
 // TODO refactor nodes to make code more reusable
 
+// TODO enhance interfaces, avoid buffering reader if it's only one node in next.
+
 //Node A node wraps components and manage receiving transactions and forwarding transactions to next nodes.
 type Node interface {
 	// Start this node and all its next nodes to start receiving transactions
